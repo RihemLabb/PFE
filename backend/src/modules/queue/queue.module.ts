f2a226d4c08652d4e777433a71c6ae4a63a4366c
Rihem Labbaoui <57710671+RihemLabb@users.noreply.github.com>
2026-08-08 22:19:43 +0100

    refactor queue module imports

--- backend/src/modules/queue/queue.module.ts
+++ backend/src/modules/queue/queue.module.ts
@@ -1,19 +1,12 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { QueueEntry, QueueEntrySchema } from './schemas/queue-entry.schema';
 import { QueueService } from './queue.service';
 import { QueueController } from './queue.controller';
+import { QueueDataModule } from './queue-data.module';
 import { AppointmentsModule } from '../appointments/appointments.module';
 import { CountersModule } from '../counters/counters.module';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([
-      { name: QueueEntry.name, schema: QueueEntrySchema },
-    ]),
-    AppointmentsModule,
-    CountersModule,
-  ],
+  imports: [QueueDataModule, AppointmentsModule, CountersModule],
   controllers: [QueueController],
   providers: [QueueService],
 })
