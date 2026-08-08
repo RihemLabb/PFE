f8e5c9b1e1f2b6230ff9c9df60e9dbc9580ba9c1
Rihem Labbaoui <57710671+RihemLabb@users.noreply.github.com>
2026-08-08 22:16:05 +0100

    add public availability endpoint

---
+++ backend/src/modules/appointments/availability.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AppointmentsService } from './appointments.service';
+import { AvailabilityQueryDto } from './dto/availability-query.dto';
+
+@ApiTags('Availability')
+@Controller('availability')
+export class AvailabilityController {
+  constructor(private readonly appointmentsService: AppointmentsService) {}
+
+  @Get()
+  @ApiOperation({ summary: 'Get available slots for a service and date' })
+  getAvailability(@Query() query: AvailabilityQueryDto) {
+    return this.appointmentsService.getAvailability(query.serviceId, query.date);
+  }
+}
